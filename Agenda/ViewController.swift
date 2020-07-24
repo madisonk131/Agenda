6a79a437005feb7db2faf3586ba6ed6f929e3a8a
madisonk131 <madisonklee131@gmail.com>
2020-07-24 13:58:19 -0400

    First Commit

--- Agenda/ViewController.swift
+++ Agenda/ViewController.swift
@@ -6,15 +6,25 @@
 //  Copyright © 2020 Madison Lee. All rights reserved.
 //
 
+import FSCalendar
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate {
 
+    @IBOutlet var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calendar.delegate = self
     }
 
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
 
 }
 
